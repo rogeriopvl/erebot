cad48f6d83388c82ca38120a3476dc1c4c20b8a7
Anmo <btavares26@gmail.com>
2014-02-07 18:23:31 +0000

    Add flood protection cache cards fix parse commands fix card probability add command card

--- index.js
+++ index.js
@@ -12,7 +12,11 @@ var config = {
 var bot = new irc.Client(
     config.server,
     config.botName,
-    { channels: config.channels , floodProtection : true }
+    {
+        channels: config.channels ,
+        floodProtection : true ,
+        floodProtectionDelay : 2500
+    }
 );
 
 var say = function (msg, from, to) {
@@ -20,15 +24,15 @@ var say = function (msg, from, to) {
     bot.say(dest, msg);
 };
 
-var sets = {};
+var sets = { };
+var cards = { };
 
 var parseCommand = function (str, from, to) {
     var cmd = {};
     var parts = str.split(' ');
 
     cmd.major = parts.shift();
-    cmd.minor = parts.shift();
-    cmd.arg = parts.shift();
+    cmd.minor = parts.join( ' ' );
     cmd.from = from;
     cmd.to = to;
 
@@ -40,7 +44,7 @@ var pickCards = function ( cardList , n ) {
 
     var idx;
     var idxs = [ ];
-    var cards = [ ];
+    var cardsArr = [ ];
     var l = cardList.length;
 
     for ( var i = 0; i < n; i++) {
@@ -51,56 +55,72 @@ var pickCards = function ( cardList , n ) {
         }
 
         idxs.push( idx );
-        cards.push( cardList[ idx ] );
+        cardsArr.push( cardList[ idx ] );
     }
 
-    return cards;
+    return cardsArr;
 };
 
 var openBooster = function ( setName ) {
-    var cards = sets[ setName ];
+    var cardsList = sets[ setName ];
 
-    var token  = Math.random( ) < 0.5;
-    var foil   = Math.random( ) < 0.7;
-    var mythic = Math.random( ) < 0.8;
+    var token  = Math.random( ) > 0.5;
+    var foil   = Math.random( ) > 0.7;
+    var mythic = Math.random( ) > 0.8;
 
-    say( '!' + pickCards( cards.Land )[ 0 ].name );
+    say( '!card ' + pickCards( cardsList.Land )[ 0 ].name );
 
-    foil && say( 'foil: ' + pickCards( cards.cards )[ 0 ].name );
+    foil && say( 'foil:\n!card ' + pickCards( cardsList.cards )[ 0 ].name );
 
-    say( '!' + pickCards( cards[ mythic ? 'Mythic Rare' : 'Rare' ] )[ 0 ].name );
+    say( '!card ' + pickCards( cardsList[ mythic ? 'Mythic Rare' : 'Rare' ] )[ 0 ].name );
 
-    var U = pickCards( cards.Uncommon , 3 );
-    say( '!' + U[ 0 ].name );
-    say( '!' + U[ 1 ].name );
-    say( '!' + U[ 2 ].name );
+    var U = pickCards( cardsList.Uncommon , 3 );
+    say( '!card ' + U[ 0 ].name );
+    say( '!card ' + U[ 1 ].name );
+    say( '!card ' + U[ 2 ].name );
 
-    var C = pickCards( cards.Common , foil ? 9 : 10 )
-    say( '!' + C[ 0 ].name );
-    say( '!' + C[ 1 ].name );
-    say( '!' + C[ 2 ].name );
-    say( '!' + C[ 3 ].name );
-    say( '!' + C[ 4 ].name );
-    say( '!' + C[ 5 ].name );
-    say( '!' + C[ 6 ].name );
-    say( '!' + C[ 7 ].name );
-    say( '!' + C[ 8 ].name );
-    !foil && say( '!' + C[ 9 ].name );
+    var C = pickCards( cardsList.Common , foil ? 9 : 10 )
+    say( '!card ' + C[ 0 ].name );
+    say( '!card ' + C[ 1 ].name );
+    say( '!card ' + C[ 2 ].name );
+    say( '!card ' + C[ 3 ].name );
+    say( '!card ' + C[ 4 ].name );
+    say( '!card ' + C[ 5 ].name );
+    say( '!card ' + C[ 6 ].name );
+    say( '!card ' + C[ 7 ].name );
+    say( '!card ' + C[ 8 ].name );
+    !foil && say( '!card ' + C[ 9 ].name );
 };
 
-var setSet = function ( setName , cards ) {
+var cardInfo = function( cardName ) {
+    var card = cards[ cardName ];
+
+    say( card.name + '\t\t\t' + ( card.mana_cost || '' ) + '\n' +
+         card.types + '\t\t\t' + ( card.rarity ? card.rarity : '' ) + '\n' +
+         card.text + '\n' +
+         ( card.power ? '\t\t\t' + card.power + '/' + card.toughness + '\n' : '' ) +
+         ( card.devotion || '' ) );
+};
+
+var setSet = function ( setName , cardsList ) {
     sets[ setName ] = {
-        cards         : cards ,
+        cards         : cardsList ,
         'Mythic Rare' : [ ] ,
         Rare          : [ ] ,
         Uncommon      : [ ] ,
         Common        : [ ] ,
-        Land          : [ ]
+        Land          : [ { name : 'Plains' } , { name : 'Island' } , { name : 'Swamp' } , { name : 'Mountain' } , { name : 'Forest' } ]
     };
 
-    for ( var i = 0, l = cards.length; i < l; i++ ) {
-        sets[ setName ][ cards[ i ].rarity ].push( cards[ i ] );
+    for ( var i = 0, l = cardsList.length; i < l; i++ ) {
+        sets[ setName ][ cardsList[ i ].rarity ].push( cardsList[ i ] );
+
+        !cards[ cardsList[ i ].name ] && setCard( cardsList[ i ].name , cardsList[ i ] );
     }
+};
+
+var setCard = function( cardName , card ) {
+    cards[ cardName ] = card;
 };
 
 var executeCmd = function (cmd) {
@@ -114,20 +134,38 @@ var executeCmd = function (cmd) {
 
             var setName = cmd.minor;
 
-            sets[ setName ] ? openBooster( setName ) : tutor.set( setName , function ( err , cards ) {
-                if ( err || cards.length < 2 ) { // somehow this returns cards = [null] lulz
+            sets[ setName ] ? openBooster( setName ) : tutor.set( setName , function ( err , cardsList ) {
+                if ( err || cardsList.length < 2 ) { // somehow this returns cardsList = [null] lulz
                     say( 'Wrong set name?' );
 
                     return;
                 }
 
-                setSet( setName , cards );
+                setSet( setName , cardsList );
 
                 openBooster( setName );
             });
             break;
-        default:
-            say('Unknown command.');
+        case 'card':
+            if (!cmd.minor) {
+                say('Usage: !card <card name>');
+
+                break;
+            }
+
+            var cardName = cmd.minor;
+
+            cards[ cardName ] ? cardInfo( cardName ) : tutor.card(cmd.minor, function (err, card) {
+                if ( err ) {
+                    say( 'Card not found!' );
+
+                    return;
+                }
+
+                setCard( cardName , card );
+
+                cardInfo( cardName );
+            });
             break;
     }
 };
